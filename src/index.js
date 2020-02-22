062ae23096cabc4a826c68541176f56277d230f0
Philliph Drummond <oilsinwater@gmail.com>
2020-02-21 21:54:46 -0800

    mend

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'mobx-react';
-import RentalDataStore from './stores/RentalDataStore';
+import RentalDataStore from './RentalDataStore';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 import "./style.css"
