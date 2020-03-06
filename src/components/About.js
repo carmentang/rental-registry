b35d88cf72a832ab3ed8cf85dbd1efc823a0395b
Philliph Drummond <oilsinwater@gmail.com>
2020-03-05 18:38:31 -0800

    feat: logo, header formating and link to Home

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,32 @@
 import React from 'react';
+
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import TechEquityLogo from '../images/tech-equity.png';
+
+
+
 
 const About = () => {
+
+
   return <div class="wrapper">
     <Header />
     <div className="homepage">
       <h1 className="homepage-title">About</h1>
       <hr />
       <p className="homepage-body">
-        <span>Blurb</span>
+        <span> * Blurb on AB 724*</span>
       </p>
+      <br></br>
+      <p className="homepage-body">
+        <span> * Blurb about Tech Equity and Volunteers*</span>
+      </p>
+      <div>
+      <img style={{ height: 100 }} alt="Tech Equity Collaborative Logo" src={TechEquityLogo}></img>
+      </div>
     </div>
+    
     <Footer />
   </div>
 }
